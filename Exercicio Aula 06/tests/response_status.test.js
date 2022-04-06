948ef5e418e2d22c3b5152f679635567b58249b1
Vinicius <viniciusdeoliveira16@hotmail.com>
2022-04-05 23:34:14 -0300

    Submetendo aula 07

--- Exercicio Aula 06/tests/response_status.test.js
+++ Exercicio Aula 06/tests/response_status.test.js
@@ -1,12 +1,22 @@
 const Utils = require('../src/utils/utils');
 const Constants = require('../src/utils/Constants');
 
+test('Status Code BAD REQUEST - Case Error Validation', () => {
+    const result = Utils.responseStatus(Constants.ErrorValidation.name);
+    expect(result).toEqual(400);
+});
+
 test('Status Code OK - Case undefined', () => {
     const result = Utils.responseStatus(undefined);
     expect(result).toEqual(200);
 });
 
-test('Status Code BAD REQUEST - Case Error Validation', () => {
-    const result = Utils.responseStatus(Constants.ErrorValidation.name);
-    expect(result).toEqual(400);
+test('Status Code NOT FOUND - Case Error NotFound', () => {
+    const result = Utils.responseStatus(Constants.ErrorNotFound.name);
+    expect(result).toEqual(404);
+});
+
+test('Status Code CONFLICT - Case Error Duplicate', () => {
+    const result = Utils.responseStatus(Constants.ErrorDuplicate.name);
+    expect(result).toEqual(409);
 });
--- Exercicio Aula 06/tests/validation.test.js
+++ Exercicio Aula 06/tests/validation.test.js
@@ -1,19 +1,19 @@
 const Constants = require('../src/utils/Constants');
 const Validation = require('../src/utils/validation');
 
-test('Caso válido', () => {
+test('Invalid case', () => {
     const result = Validation.create({
-        nome: "Rafaela",
-        email: "rafaela.papale@gec.inatel.br",
-        senha: "123456789"
+        nome: "Pessoa",
+        forca: "10",
+        poder: "100%",
     });
     expect(result).toEqual(undefined);
 });
 
-test('Caso inválido - Retirando nome', () => {
+test('Invalid case - without forca', () => {
     const result = Validation.create({
-        email: "rafaela.papale@gec.inatel.br",
-        senha: "123456789"
+        nome: "Pessoa",
+        poder: "100%",
     });
     expect(result.name).toEqual(Constants.ErrorValidation.name);
 });
